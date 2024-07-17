e709da2abe6b41a4b1eae181701303e999cc31ea
Narethputponleu Hong <hongnarethputponleu015@gmail.com>
2024-07-17 09:51:49 +0700

    Add SCSS

--- app/feeds/[id]/Render.tsx
+++ app/feeds/[id]/Render.tsx
@@ -5,19 +5,18 @@ import Blockquote from "@tiptap/extension-blockquote";
 import Document from "@tiptap/extension-document";
 import Paragraph from "@tiptap/extension-paragraph";
 import Text from "@tiptap/extension-text";
+import Italic from "@tiptap/extension-italic";
+import Strikethrough from "@tiptap/extension-strike";
+import { Heading } from "@tiptap/extension-heading";
+import Code from "@tiptap/extension-code";
 import Bold from "@tiptap/extension-bold";
 import CodeBlockLowlight from "@tiptap/extension-code-block-lowlight";
 import BulletList from "@tiptap/extension-bullet-list";
 import OrderedList from "@tiptap/extension-ordered-list";
 import ListItem from "@tiptap/extension-list-item";
-
-import {
-  NodeHandler,
-  NodeHandlers,
-  TipTapRender,
-} from "@troop.com/tiptap-react-render";
 import { useMemo } from "react";
 import { common, createLowlight } from "lowlight";
+import { mergeAttributes } from "@tiptap/react";
 type Props = {
   data: any[] | null;
 };
@@ -27,8 +26,30 @@ export default async function Render(props: Props) {
       Document,
       Paragraph,
       Text,
+      Code,
       Bold,
       Blockquote,
+      Strikethrough,
+      Italic,
+      Heading.configure({ levels: [1, 2] }).extend({
+        levels: [1, 2],
+        renderHTML({ node, HTMLAttributes }) {
+          const level = this.options.levels.includes(node.attrs.level)
+            ? node.attrs.level
+            : this.options.levels[0];
+          const classes: { [key: number]: string } = {
+            1: "text-4xl",
+            2: "text-2xl",
+          };
+          return [
+            `h${level}`,
+            mergeAttributes(this.options.HTMLAttributes, HTMLAttributes, {
+              class: `${classes[level]}`,
+            }),
+            0,
+          ];
+        },
+      }),
       CodeBlockLowlight.configure({
         lowlight: createLowlight(common),
         languageClassPrefix: "language-css",
--- data/server/note.ts
+++ data/server/note.ts
@@ -2,12 +2,12 @@ import { createClient } from "@/lib/supabase/server";
 
 export async function getNoteById(id: number) {
   const supabase = createClient();
-  const { data, error } = await supabase.from("Notes").select("*").eq("id", id);
+  const { data, error } = await supabase.from("notes").select("*").eq("id", id);
   return { data, error };
 }
 
 export async function getAllNotes() {
   const supabase = createClient();
-  const { data, error } = await supabase.from("Notes").select("*");
+  const { data, error } = await supabase.from("notes").select("*");
   return { data, error };
 }

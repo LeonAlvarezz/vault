386c2fbffe3bd3cd0b25b174492f02b222f8e111
Narethputponleu Hong <hongnarethputponleu015@gmail.com>
2024-08-20 00:09:32 +0700

    remove log

--- app/api/action.ts
+++ app/api/action.ts
@@ -6,18 +6,14 @@ import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
 export async function login(formData: FormData) {
-  // console.log(formData.values);
   const supabase = createClient();
 
-  // type-casting here for convenience
-  // in practice, you should validate your inputs
   const data = {
     email: formData.get("email") as string,
     password: formData.get("password") as string,
   };
 
   const { error } = await supabase.auth.signInWithPassword(data);
-  console.log("error:", error);
 
   if (error) {
     redirect("/error");
@@ -49,7 +45,6 @@ export async function signup(formData: FormData) {
       ]);
 
     if (profileError) {
-      console.log("profileError:", profileError);
       redirect("/error");
     }
   }

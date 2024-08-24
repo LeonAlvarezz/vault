06ba95b9a6d2aa51f03c11c0875d938c94eb2499
Narethputponleu Hong <hongnarethputponleu015@gmail.com>
2024-08-24 13:23:14 +0700

    Remove unused

--- components/ui/Render.tsx
+++ components/ui/Render.tsx
@@ -1,13 +1,11 @@
 import { BlockNode, Note, NOTE_CONTENT_TYPE } from "@/types/note.type";
 import { renderNote } from "@/lib/renderNote";
-import mockData from "@/mock-data/note.json";
 type Props = {
   data?: any[] | null;
   // id: string;
   note: Note;
 };
 export default async function Render({ data, note }: Props) {
-  // const notes = mockData.note.filter((data) => data.id == +id);
   return (
     <div className="tiptap dark:text-white text-slate-800 flex flex-col gap-2">
       {note.content.content.map((block, blockIndex) => (
